8098a3d8a2b2e794363bcc58a85c14454017f5a9
win <win@192.168.1.12>
2020-06-30 16:31:56 -0500

    Preg001 _ Prog004 - Listo

---
+++ src/estructuras/Prog004.java
@@ -0,0 +1,13 @@
+
+package estructuras;
+
+public class Prog004 {
+    public static void main(String[] args) {
+        int numero=1;
+        String mensaje = "Crito viene pronto Preparate amen";
+        while(numero<=10){
+            System.out.println(mensaje);
+            numero++;
+        }
+    }
+}
