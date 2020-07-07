27580ddcd06ddd4399108fba2b38374f3896beba
win <win@192.168.1.12>
2020-07-07 14:13:41 -0500

    Prog 007 - Metodo vacio

---
+++ src/modularidad/Prog007.java
@@ -0,0 +1,20 @@
+package modularidad;
+
+public class Prog007 {
+    //Metodo vacio
+    //Vacio porque dentro del parentesis esta vacio..
+    public static void saludo() {
+        System.out.println("Bienvenido al Sistema");
+    }
+    public static void despedido() {
+        System.out.println("Gracias por usar este programa");
+    }
+    public static void mensaje() {
+        System.out.println("Si puedes imaginarlo puedes programarlo");
+    }
+    public static void main(String[] args) {
+        saludo();
+        mensaje();
+        despedido();
+    }
+}
